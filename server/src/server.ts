fc82dc7af9f89ec5cec111736ea5eb34fba09fce
Caio <caio.student@outlook.com>
2020-06-02 17:49:26 -0300

    iniciando backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,48 @@ import express from 'express';
 
 const app = express()
 
+app.use(express.json())
+
+const users = [
+    'Diego',
+    'Caio',
+    'Rodrigo',
+    'Lucas'
+];
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredUsers =search ? users.filter(user => user.includes(search)) : users
+
+    return response.json(filteredUsers)
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
 app.get('/users', (request, response) => {
     console.log('Listagem de usuário');
 
-    response.json([
-        'Diego',
-        'Caio',
-        'Rodrigo',
-        'Lucas'
-    ])
+    return response.json(users);
 });
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    console.log(data);
+
+    const user = {
+        name: data.name,
+        email: data.email,
+    };
+
+    return response.json(user)
+})
 
 app.listen(3333);
