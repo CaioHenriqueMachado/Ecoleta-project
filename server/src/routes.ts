bc53aea786c7b3f7bec10e7336b2fea6b1320b21
Caio <caio.student@outlook.com>
2020-06-02 23:45:57 -0300

    finalização backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,24 @@
 import express from 'express';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
-const routes = express.Router()
 
-routes.get('/', (request, response) => {
-    return response.json({message:'Helo'});
-});
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+// PARA LISTA ITEMS
+routes.get('/items', itemsController.index);
+
+// CRIAR UM NOVO PONTO DE COLETA
+routes.post('/points', pointsController.create);
+
+// LISTAR PONTOS
+routes.get('/points', pointsController.index);
+
+// LISTA PONTO ESPECIFICO
+routes.get('/points/:id', pointsController.show);
+
 
 export default routes;
 
